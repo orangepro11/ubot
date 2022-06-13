72161df7781d264cd66f268cc24f27bbef1a5524
不爱喝橙子汁 <201410730128@stu.shmtu.edu.cn>
2022-06-13 13:36:45 +0800

    feature: register http client module

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core'
 import { CoreModule, bootstrap } from '@akunx/nest-core'
 import { SharedModule } from './shared/module'
 import { ServiceModule } from './service/module'
+import { HttpClientModule } from '@tresdoce/nestjs-httpclient'
 
 bootstrap({
   port: 8000,
   application: NestFactory.create(
     CoreModule.register({
-      imports: [SharedModule, ServiceModule]
+      imports: [SharedModule, ServiceModule, HttpClientModule]
     })
   )
 })
