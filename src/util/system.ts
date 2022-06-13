e79eaedbe388bae53b71823f28f4a4995c019653
不爱喝橙子汁 <201410730128@stu.shmtu.edu.cn>
2022-06-13 11:07:54 +0800

    feature: add arguments when exec ping command

--- src/util/system.ts
+++ src/util/system.ts
@@ -1,8 +1,8 @@
 export function ping(host = 'www.uviewui.com'): Promise<string> {
-  return new Promise((resolve,reject) => {
-    let ping = require('child_process').spawn('ping', host);
-    let iconv = require('iconv-lite');
-    let all = ""
+  return new Promise((resolve, reject) => {
+    let ping = require('child_process').spawn('ping', ['-c', '3', '-t', '3', '-W', '3', host])
+    let iconv = require('iconv-lite')
+    let all = ''
     ping.stdout.on('data', data => {
       all += iconv.decode(data, 'cp936')
     })
@@ -13,5 +13,4 @@ export function ping(host = 'www.uviewui.com'): Promise<string> {
       resolve(all)
     })
   })
- 
 }
